240676d7291ce0557fd780ac9228b2b96c62726b
EfratVardi <211.efrat@gmail.com>
2025-01-20 01:09:29 +0200

    end

--- DAL/Interfaces/Calculate.cs
+++ DAL/Interfaces/Calculate.cs
@@ -5,15 +5,5 @@ namespace DAL.Interfaces;
 
 public interface ICalculateDal
 {
-    //Task<List<sal>> GetAll();
-
-    //Task<Game> Add(Game games);
-
-    //Task<Game> Update(Game games);
-
-    //Task<bool> Delete(int id);
-
-    //Task<Game> GetById(int id);
-
-    //Task<List<Game>> GetByUserId(int userId);
+   
 }
